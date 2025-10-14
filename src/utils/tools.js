87c9b310a78201351c43befd426e84348a3824f7
Julia <uug11@mail.ru>
2025-10-14 18:49:20 +0400

    Feat: new page for subsciption

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -32,3 +32,13 @@ export function showVersion() {
     process.env.VUE_APP_GIT_SHA
   );
 }
+
+export function fromWei(amount, decimals, fixed = 4) {
+  const wei = BigInt(amount);
+  const value = Number(wei) / Math.pow(10, decimals);
+  return value.toFixed(fixed);
+}
+export function toWei(value, decimals) {
+  const weiValue = BigInt(Math.floor(Number(value) * Math.pow(10, decimals)));
+  return weiValue.toString();
+}
